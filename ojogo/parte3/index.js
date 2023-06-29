ab60e140f5f0304a9f84e156f76a7fb8d4bdd026
LeonardoASC <leonardoaschaves@gmail.com>
2023-06-29 11:52:05 -0300

    parte 3 mudei

--- ojogo/parte3/index.js
+++ ojogo/parte3/index.js
@@ -1,13 +1,56 @@
+var charadas = [
+  { pergunta: "Qual é o animal mais antigo do mundo?", resposta: "Tartaruga" },
+  { pergunta: "O que é que quanto maior menos se vê?", resposta: "Escuro" },
+  { pergunta: "O que é o que é, uma casinha branca com muitos bichinhos dentro?", resposta: "Fogão" },
+  { pergunta: "Qual é a palavra que os homens usam frequentemente mas as mulheres odeiam?", resposta: "Calma" },
+  { pergunta: "O que é que sempre se quebra ao ser falado?", resposta: "Silêncio" }
+];
 
-document.getElementById("answer").addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    var answer = this.value.toLowerCase();
-    if (answer === "morcego") {
-      document.getElementById("message").textContent = "Resposta correta! Parabéns, você passou para a próxima tela.";
-      window.location.href = "../parte4/index.html";
+var charadaIndex = 0;
+var respostas = [];
+
+function verificarResposta() {
+  var inputResposta = document.getElementById("resposta");
+  var resposta = inputResposta.value;
+
+  if(resposta.toLowerCase() === charadas[charadaIndex].resposta.toLowerCase()) {
+    respostas.push({ pergunta: charadas[charadaIndex].pergunta, resposta: resposta });
+    charadaIndex++;
+
+    if (charadaIndex < charadas.length) {
+      inputResposta.value = ""; // Limpa o campo de resposta
+      exibirCharada();
     } else {
-      document.getElementById("message").textContent = "Resposta incorreta. Tente novamente.";
-      this.value = "";
+      mostrarRespostas();
     }
+  } else {
+    alert("Resposta incorreta. Tente novamente!");
   }
-});
+}
+
+function exibirCharada() {
+  var charadaContainer = document.getElementById("charadaContainer");
+  var charada = charadas[charadaIndex].pergunta;
+
+  charadaContainer.innerHTML = `
+    <p id="charada">${charada}</p>
+    <label for="resposta">Resposta:</label>
+    <input type="text" id="resposta" required>
+  
+    <button onclick="verificarResposta()">Verificar</button>
+  `;
+}
+
+function mostrarRespostas() {
+  var respostasDiv = document.getElementById("respostas");
+  respostasDiv.innerHTML = "";
+
+  respostas.forEach(function(item) {
+    var p = document.createElement("p");
+    p.innerHTML = "<strong>" + item.pergunta + ":</strong> " + item.resposta;
+    respostasDiv.appendChild(p);
+  });
+}
+
+// Inicializa a exibição da primeira charada
+exibirCharada();
